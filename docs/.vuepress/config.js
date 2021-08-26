c50bebcf4538923c24f0ae23e3ad8a50f227d46c
singcl <1276837689@qq.com>
2021-08-26 18:38:53 +0800

    :memo: 基于vuepress1.x和vuepress-theme-reco主题的文档网站

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'Imcoco.top 官方文档',
+  description: 'Imcoco.top，一个专业的建站神器，已有超过10万+网站使用 Imcoco.top 搭建',
+  // base:'/docs/',
+  markdown: {
+    lineNumbers: true,
+  },
+  theme: 'vuepress-theme-reco',
+};
